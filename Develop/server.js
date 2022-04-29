98336daa0a7e901122f5b1f92b29ef730ba182ba
Kalif Purce <kalifpurce@gmail.com>
2022-04-28 20:05:30 -0700

    logic not working

--- Develop/server.js
+++ Develop/server.js
@@ -1,33 +1,31 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for feedback page
-app.get('/api/notes', (req, res) =>
-  res.readFile(path.join(__dirname, '/db.json'))
+app.get("/api/notes", (req, res) =>
+  res.readFile(path.join(__dirname, "/db.json"))
 );
 
-app.post('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db.json')
-)});
-    
-  app.listen(PORT, function(err){
-      if (err) console.log(err);
-      console.log("Server listening on PORT", PORT);
-  }); 
+app.post("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db.json"));
+});
 
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
